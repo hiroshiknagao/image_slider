7bc6dda4f95f7ab3481c706f8191bed4a960acd1
hiroshi nagao <hi_1205@hotmail.com>
2024-06-09 11:57:24 +0900

    start all over again cuz of JS not working

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
-const imageSlide = document.querySelector('.image-slide');
-const images = document.querySelectorAll('.image-slide img')
-//buttom
-const preBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
-//counter
-let counter = 1;
-const size = images[0].clientWidth;
+// image slider
 
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId = null;
 
-imageSlide.style.transform = 'translatex(' + (-size * counter ) + 'px)';
-//button listenr
+initializeSlider();
+function initializeSlider(){
 
-nextBtn.addEventListener('click', () => {
-  if (counter >= images.length -1) return;
-  imageSlide.style.transition = "transfrom 0.4s ease-in-out";
-  counter++;
-  imageSlide.style.transform = 'translatex(' + (-size * counter ) + 'px)';
-});
+  slides[slideIndex].classList.add("displaySlide");
 
-preBtn.addEventListener('click', () => {
-  if (counter <= 0) return;
-  imageSlide.style.transition = "transfrom 0.4s ease-in-out";
-  counter--;
-  imageSlide.style.transform = 'translatex(' + (-size * counter ) + 'px)';
-});
+}
 
-imageSlide.addEventListener('transitionend', () => {
-  if (images[counter].id === 'lastClone') {
-    imageSlide.style.transition = "none";
-    counter = images.length - 2;
-    imageSlide.style.transform = 'translatex(' + (-size * counter ) + 'px)';
-  }
-  if (images[counter].id === 'firstClone') {
-    imageSlide.style.transition = "none";
-    counter = images.length - counter;
-    imageSlide.style.transform = 'translatex(' + (-size * counter ) + 'px)';
-  }
-});
+function showSlider(index){
+
+}
+
+function prevSlide() {
+
+}
+
+function nextSlide() {
+
+}
